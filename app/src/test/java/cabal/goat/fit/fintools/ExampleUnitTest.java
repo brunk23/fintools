424547ba473078f9354e3d30930aa31c95a02667
Goat Boy <Goat Boy>
2016-03-27 21:52:38 -0400

    Create the original empty project

---
+++ app/src/test/java/cabal/goat/fit/fintools/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package cabal.goat.fit.fintools;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
